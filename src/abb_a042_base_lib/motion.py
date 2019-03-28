a10dc23e2c63b333985ffe8f043ff492131cf753
fleischp <fleischmann@arch.ethz.ch>
2019-03-28 10:00:44 +0100

    moveJ added

--- src/abb_a042_base_lib/motion.py
+++ src/abb_a042_base_lib/motion.py
@@ -53,7 +53,7 @@ class MoveAbsJ(ROSmsg):
 
         zone (:class:`Zone`):
             Zone data. Predefined in the robot contrller,
-            only Zone ``fine`` will to a stop point all others are fly by points
+            only Zone ``fine`` will do a stop point all others are fly by points
 
         feedback_level (:obj:`int`):
             Integer specifying requested feedback level. Default=``0`` (i.e. ``NONE``).
@@ -93,6 +93,69 @@ class MoveAbsJ(ROSmsg):
         ext_axes_pad = [0.0] * (6 - len(ext_axes))
         self.string_values = []
         self.float_values = joints + joints_pad + ext_axes + ext_axes_pad + [speed, zone]
+
+
+class MoveJ(ROSmsg):
+    """Represents a move joint instruction.
+
+    Attributes:
+
+        pos (:obj:`list` of :obj:`float`):
+            Translation X, Y and Z positions in milimeters.
+
+        rot (:obj:`list` of :obj:`float`):
+            Rotation Q1, Q2, Q3 and Q4 in quatarnions.
+
+        ext_axes (:obj:`list` of :obj:`float`):
+            External axes positions, depending on the robotic system,
+            it can be millimeters for prismatic external axes,
+            or degrees for revolute external axes.
+
+        speed (:obj:`int`):
+            Integer specifying TCP translational speed in mm/s. Min=``0.01``.
+
+        zone (:class:`Zone`):
+            Zone data. Predefined in the robot contrller,
+            only Zone ``fine`` will do a stop point all others are fly by points
+
+        feedback_level (:obj:`int`):
+            Integer specifying requested feedback level. Default=``0`` (i.e. ``NONE``).
+            Feedback level is instruction-specific but the value ``1`` always represents
+            completion of the instruction.
+
+
+    ABB Documentation - Usage:
+
+        MoveJ is used to move the robot quickly from one point to another when that
+        movement does not have to be in a straight line.
+        The robot and external axes move to the destination position along a non-linear
+        path. All axes reach the destination position at the same time.
+        This instruction can only be used in the main task T_ROB1 or, if in a MultiMove
+        system, in Motion tasks.
+
+    """
+
+    def __init__(self, pos, rot, ext_axes, speed, zone, feedback_level=MotionFeedback.NONE):
+        self.instruction = INSTRUCTION_PREFIX + 'MoveJ'
+        self.feedback_level = feedback_level
+        self.exec_level = ExecutionLevel.ROBOT
+
+        pos = pos or []
+        if len(pos) != 3:
+            raise ValueError('X, Y, Z, values are needed')
+
+        rot = rot or []
+        if len(rot) != 4:
+            raise ValueError('Q1, Q2, Q3, Q4 values are needed')
+        # maybe check the quartinions sum ?
+
+        ext_axes = ext_axes or []
+        if len(ext_axes) > 6:
+            raise ValueError('Only up to 6 external axes are supported')
+        ext_axes_pad = [0.0] * (6 - len(ext_axes))
+
+        self.string_values = []
+        self.float_values = pos + rot + ext_axes + ext_axes_pad + [speed, zone]
 
 
 if __name__ == '__main__':
