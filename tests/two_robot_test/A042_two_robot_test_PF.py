b4e382d70087f8fb72d023ff60dfa7f9911c875e
fleischp <fleischmann@arch.ethz.ch>
2019-10-04 15:46:22 +0200

    first test after name change to compas_rrc

--- tests/two_robot_test/A042_two_robot_test_PF.py
+++ tests/two_robot_test/A042_two_robot_test_PF.py
@@ -1,4 +1,4 @@
-from abb_a042_base_lib import *
+from compas_rrc import *
 from compas.geometry import Frame
 from compas_fab.backends.ros import RosClient
 import re
@@ -9,19 +9,18 @@ if __name__ == '__main__':
     ros = RosClient()
     ros.run()
 
-    abb_r1 = AbbClient(ros, 'robot_1')
-    abb_r2 = AbbClient(ros, 'robot_2')
+    abb_r1 = AbbClient(ros, '/robot_1')
+    abb_r2 = AbbClient(ros, '/robot_2')
 
     print('Connected.')
 
-
     # move robot one
-    abb_r1.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [28000, -3200, -2500], 500, Zone.FINE, feedback_level=1))
-    abb_r1.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [28000, -4200, -2500], 500, Zone.FINE, feedback_level=1))
+    abb_r1.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [8000, -3200, -2500], 500, Zone.FINE, feedback_level=1))
+    abb_r1.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [8000, -4200, -2500], 500, Zone.FINE, feedback_level=1))
     print('Finished 1')
 
-    abb_r2.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [28000, -6200, -2500], 500, Zone.FINE, feedback_level=1))
-    abb_r2.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [28000, -7200, -2500], 500, Zone.FINE, feedback_level=1))
+    abb_r2.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [8000, -6200, -2500], 500, Zone.FINE, feedback_level=1))
+    abb_r2.send_and_wait(MoveAbsJ([90, -55, 45, 180, -10, 45], [8000, -7200, -2500], 500, Zone.FINE, feedback_level=1))
     print('Finished 2')
 
     # end of code
