d90e4e7d64c1e3fea440c2113db3b1cd65932b7e
fleischp <fleischmann@arch.ethz.ch>
2019-10-15 17:26:36 +0200

    GetJointT added and started to test

--- src/compas_rrc/utility.py
+++ src/compas_rrc/utility.py
@@ -16,6 +16,14 @@ class Dummy(ROSmsg):
         self.string_values = []
         self.float_values = []
 
+class GetJointT(ROSmsg):
+    def __init__(self, feedback_level=UtilityFeedback.NONE):
+        self.instruction = INSTRUCTION_PREFIX + 'GetJointT'
+        self.feedback_level = feedback_level
+        self.exec_level = ExecutionLevel.ROBOT
+        self.string_values = []
+        self.float_values = []
+
 class SetAcc(ROSmsg):
     def __init__(self, acc, ramp, feedback_level=UtilityFeedback.NONE):
         self.instruction = INSTRUCTION_PREFIX + 'SetAcc'
