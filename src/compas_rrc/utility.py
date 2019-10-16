270d43a8b99acd3ee5a7ca9f928afae61b1934f8
fleischp <fleischmann@arch.ethz.ch>
2019-10-16 15:18:22 +0200

    test parse_feedback with GetJointT

--- src/compas_rrc/utility.py
+++ src/compas_rrc/utility.py
@@ -24,6 +24,28 @@ class GetJointT(ROSmsg):
         self.string_values = []
         self.float_values = []
 
+    def parse_feedback(self, result):
+
+        # read robot jonts
+        robot_joint_1 = round(result['float_values'][18],2)
+        robot_joint_2 = round(result['float_values'][19],2)
+        robot_joint_3 = round(result['float_values'][20],2)
+        robot_joint_4 = round(result['float_values'][21],2)
+        robot_joint_5 = round(result['float_values'][22],2)
+        robot_joint_6 = round(result['float_values'][23],2)
+        robot_joint = [robot_joint_1,robot_joint_2,robot_joint_3,robot_joint_4,robot_joint_5,robot_joint_6]
+
+        # read gantry joints
+        gantry_joint_x = round(result['float_values'][24],2)
+        gantry_joint_y = round(result['float_values'][25],2)
+        gantry_joint_z = round(result['float_values'][26],2)
+        gantry_joint = [gantry_joint_x, gantry_joint_y, gantry_joint_z]
+
+        # write result
+        result = [robot_joint, gantry_joint]
+        return result
+
+
 class GetRobT(ROSmsg):
     def __init__(self, feedback_level=UtilityFeedback.NONE):
         self.instruction = INSTRUCTION_PREFIX + 'GetRobT'
