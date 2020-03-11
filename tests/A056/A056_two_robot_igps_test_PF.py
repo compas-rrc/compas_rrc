181a0b63acf9c59b044425fa65ab1f89cc96f667
fleischp <fleischmann@arch.ethz.ch>
2020-03-11 11:10:55 +0100

    A056 iGPS test done with Lukas Stadelmann

--- tests/A056/A056_two_robot_igps_test_PF.py
+++ tests/A056/A056_two_robot_igps_test_PF.py
@@ -37,14 +37,39 @@ if __name__ == '__main__':
     # iGPS test robot 1
     if on:
 
+        # Connect with iGPS Server
+        string_values = ['Robot11']
+        float_values = []
+        feedback_level = 0
+        abb_r1.send(CustomInstruction('r_A042_iGPS_Connect', string_values, float_values, feedback_level=feedback_level))
+
+        # Set Tool
+        abb_r1.send(SetTool('tT11TimGr1'))
+
+        # Set Work Object
+        abb_r1.send(SetWorkObject('igps_wobj0'))
+
+        # Set iGPS tool
+        string_values = ['tT11TimGr1']
+        float_values = []
+        feedback_level = 0
+        abb_r1.send(CustomInstruction('r_A042_iGPS_SetTool', string_values, float_values, feedback_level=feedback_level))
+
+        # Set iGPS work object
+        string_values = ['wobj0']
+        float_values = []
+        feedback_level = 0
+        abb_r1.send(CustomInstruction('r_A042_iGPS_SetWobj', string_values, float_values, feedback_level=feedback_level))
+
         # Get frame
         frame, external_axes = abb_r1.send_and_wait(GetRobtarget())
+        print('GetRobtarget Result = ', frame.point, frame.quaternion , external_axes)
 
-        # Print received values
-        print(frame, external_axes)
-
-        # Connect with iGPS Server
-        abb_r1.send(CustomInstruction('r_A042_iGPS_Connect',['Robot11'],[]))
+        # iGPS move and correct
+        string_values = []
+        float_values = list(frame.point) + list(frame.quaternion) + list(external_axes) + [0.0, 0.0, 0.0] + [100.0] + [Zone.FINE]
+        feedback_level = 1
+        done = abb_r1.send_and_wait(CustomInstruction('r_A042_iGPS_MoveLAndCorrect', string_values, float_values, feedback_level=feedback_level))
 
     # end of code
     print('Finished')
