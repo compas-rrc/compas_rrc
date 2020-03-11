665e070ce1fc13cdf7057cec3a0247e703d2434a
fleischp <fleischmann@arch.ethz.ch>
2020-03-11 14:37:08 +0100

    first multi connection done with a real and a virtual controller (c51 and vc51)

--- tests/Template/A042_multi_controller_test.py
+++ tests/Template/A042_multi_controller_test.py
@@ -4,19 +4,24 @@ from compas_fab.backends.ros import RosClient
 if __name__ == '__main__':
 
     # Create Ros Client
-    ros = RosClient()
+    ros = RosClient('192.168.125.42')
 
     # Create ABB Client
-    abb = AbbClient(ros)
-    abb.run()
+    abb_c51 = AbbClient(ros, '/c51')
+    abb_vc51 = AbbClient(ros, '/vc51')
+
+    # run the eventloop on python to connect with ros
+    ros.run()
     print('Connected.')
 
     # Print Text happy new year
-    abb.send(PrintText('IRB120 Real Controller '))
+    abb_c51.send(PrintText('IRB120 Real Controller '))
+    abb_vc51.send(PrintText('IRB120 Virtual Controller '))
 
     # End of Code
     print('Finished')
 
     # Close client
-    abb.close()
-    abb.terminate()
+    abb_c51.close()
+    abb_vc51.close()
+    ros.terminate()
