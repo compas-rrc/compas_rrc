0ed5208c03a983316451581ae354ea04bb238a09
fleischp <fleischmann@arch.ethz.ch>
2020-03-11 15:04:26 +0100

    multi controller tested with real c51 and virtual c11_r11_r12

--- tests/Template/A042_multi_controller_test.py
+++ tests/Template/A042_multi_controller_test.py
@@ -8,20 +8,24 @@ if __name__ == '__main__':
 
     # Create ABB Client
     abb_c51 = AbbClient(ros, '/c51')
-    abb_vc51 = AbbClient(ros, '/vc51')
+    abb_vc11_r11 = AbbClient(ros, '/vc11_r11')
+    abb_vc11_r12 = AbbClient(ros, '/vc11_r12')
 
     # run the eventloop on python to connect with ros
     ros.run()
     print('Connected.')
 
     # Print Text happy new year
-    abb_c51.send(PrintText('IRB120 Real Controller '))
-    abb_vc51.send(PrintText('IRB120 Virtual Controller '))
+    abb_c51.send(PrintText('C51 Robot 1 '))
+    abb_vc11_r11.send_and_wait(PrintText('vC11 Robot 11 '))
+    input("Any key")
+    abb_vc11_r12.send(PrintText('vC11_Robot 12 '))
 
     # End of Code
     print('Finished')
 
     # Close client
     abb_c51.close()
-    abb_vc51.close()
+    abb_vc11_r11.close()
+    abb_vc11_r12.close()
     ros.terminate()
