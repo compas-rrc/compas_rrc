7fb7f73baabe7a6c14f0f13868d51064b1561761
fleischp <fleischmann@arch.ethz.ch>
2020-05-06 15:19:04 +0200

    multi controller test code cleaned

--- tests/Template/A042_multi_controller_test_c51_vc51.py
+++ tests/Template/A042_multi_controller_test_c51_vc51.py
@@ -5,21 +5,18 @@ if __name__ == '__main__':
 
     # Create Ros Client
     ros = RosClient()
-    print('RosClient.')
 
     # Create ABB Client
     abb_c51 = AbbClient(ros, '/C51')
-    print('Connected C51')
     abb_vc51 = AbbClient(ros, '/vC51')
-    print('Connected vC51')
 
     # run the eventloop on python to connect with ros
     ros.run()
     print('Connected.')
 
     # Print Text happy new year
-    done = abb_c51.send_and_wait(PrintText('C51 Robot 1 '))
-    future_vc51 = abb_vc51.send_and_wait(PrintText('vC51 Robot 1 '))
+    done_c51 = abb_c51.send_and_wait(PrintText('C51 Robot 1 '))
+    done_vc51 = abb_vc51.send_and_wait(PrintText('vC51 Robot 1 '))
 
     # End of Code
     print('Finished')
