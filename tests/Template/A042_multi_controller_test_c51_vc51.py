1173e2c282807707b11d074f77c112b9fddbe1bc
fleischp <fleischmann@arch.ethz.ch>
2020-05-06 17:06:35 +0200

    multi controller test code extended with read from real and send to virtual

--- tests/Template/A042_multi_controller_test_c51_vc51.py
+++ tests/Template/A042_multi_controller_test_c51_vc51.py
@@ -14,9 +14,42 @@ if __name__ == '__main__':
     ros.run()
     print('Connected.')
 
-    # Print Text happy new year
-    done_c51 = abb_c51.send_and_wait(PrintText('C51 Robot 1 '))
-    done_vc51 = abb_vc51.send_and_wait(PrintText('vC51 Robot 1 '))
+    # Switches for Code
+    on = True
+    off = False
+
+    # Simple function check
+    if off:
+
+        # Print message on FlexPendant
+        done_c51 = abb_c51.send_and_wait(PrintText('C51 Robot 1 '))
+        done_vc51 = abb_vc51.send_and_wait(PrintText('vC51 Robot 1 '))
+
+    # Read position from real robot and and move vitrual robot in the same position
+    if on:
+
+        # read position from real robot
+        robot_joints, external_axes = abb_c51.send_and_wait(GetJoints())
+
+        # move virtual robot
+        virtual_robot_in_real_robot_positon = abb_vc51.send_and_wait(MoveToJoints(robot_joints, external_axes, 100, Zone.FINE))
+
+    # Show case
+    if off:
+
+        # Deactivate soft servo on robot
+        # abb.send(CustomInstruction('r_X000_DeactSoftRobot'))
+        def get_joint_received(result):
+            print(result)
+
+        abb.send_and_subscribe(Debug(CustomInstruction('r_A042_CyJobStart',['r_A042_GetJointT'],[0.01],exec_level=ExecutionLevel.MASTER)), get_joint_received)
+
+        # wait for user abort
+        input('Press any key to finish!')
+
+        done = abb.send_and_wait(CustomInstruction('r_A042_CyJobEnd',[],[],exec_level=ExecutionLevel.MASTER))
+        print('Cyclic job ended', done)
+
 
     # End of Code
     print('Finished')
