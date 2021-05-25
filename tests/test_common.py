96c505743d45f4f024e7264b0d4408e09908c02c
beverly <lytle@arch.ethz.ch>
2021-05-25 09:25:51 +0200

    Add tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,6 @@
 import math
 
+from compas.robots import Configuration
 import compas_rrc as rrc
 
 
@@ -40,6 +41,16 @@ def test_robot_joints():
     assert c.joint_values == [math.pi/6, -math.pi/4, 0.1]
     assert len(c.joint_names) == 0
 
+    config = Configuration([0, 1, 2, 3, 4, 5], [0, 0, 0, 0, 0, 0])
+    rj = rrc.RobotJoints.from_configuration(config)
+    assert rj.rax_1 == 0
+    assert rj.rax_6 == 5
+
+    config = Configuration([0, 1, 2, 3, 4, 5, 6], [0, 0, 0, 0, 0, 0, 0], ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+    rj = rrc.RobotJoints.from_configuration(config, ['b', 'c', 'd', 'e', 'f', 'g'])
+    assert rj.rax_1 == 1
+    assert rj.rax_6 == 6
+
 
 def test_external_axes():
     ea = rrc.ExternalAxes()
@@ -51,11 +62,11 @@ def test_external_axes():
     ea = rrc.ExternalAxes(30, 10, 0)
     assert list(ea) == [30, 10, 0]
 
-    # ea = rrc.ExternalAxes([30, 10, 0])
-    # assert list(ea) == [30, 10, 0]
-    #
-    # ea = rrc.ExternalAxes(iter([30, 10, 0]))
-    # assert list(ea) == [30, 10, 0]
+    ea = rrc.ExternalAxes([30, 10, 0])
+    assert list(ea) == [30, 10, 0]
+
+    ea = rrc.ExternalAxes(iter([30, 10, 0]))
+    assert list(ea) == [30, 10, 0]
 
     j = rrc.ExternalAxes(30, 45, 0)
     c = j.to_configuration([0, 1, 2], ['j1', 'j2', 'j3'])
@@ -66,3 +77,13 @@ def test_external_axes():
     c = j.to_configuration([0, 1, 2])
     assert c.joint_values == [math.pi/6, -math.pi/4, 0.1]
     assert len(c.joint_names) == 0
+
+    config = Configuration([0, 1, 2, 3, 4, 5], [0, 0, 0, 0, 0, 0])
+    rj = rrc.ExternalAxes.from_configuration(config)
+    assert rj.eax_a == 0
+    assert rj.eax_f == 5
+
+    config = Configuration([0, 1, 2, 3, 4, 5, 6], [0, 0, 0, 0, 0, 0, 0], ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+    rj = rrc.ExternalAxes.from_configuration(config, ['b', 'c', 'd', 'e', 'f', 'g'])
+    assert rj.eax_a == 1
+    assert rj.eax_f == 6
