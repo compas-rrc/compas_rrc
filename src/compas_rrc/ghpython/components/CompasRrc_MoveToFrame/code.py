2193d91086abb9030cea43b7b91c25d7d46aab28
Gonzalo Casas <casas@arch.ethz.ch>
2021-10-04 09:54:14 +0200

    Add first batch of GH components for RRC

---
+++ src/compas_rrc/ghpython/components/CompasRrc_MoveToFrame/code.py
@@ -0,0 +1,50 @@
+"""
+Move robot to the specified frame in cartesian space.
+"""
+from ghpythonlib.component import add_error
+from ghpythonlib.component import add_warning
+from ghpythonlib.componentbase import executingcomponent as component
+
+import compas_rrc as rrc
+from compas_rrc.ghpython import coerce_frame
+
+
+class CompasRrcMoveToFrame(component):
+    def __init__(self):
+        super(CompasRrcMoveToFrame, self).__init__()
+        self.motion_type = rrc.Motion.JOINT
+
+    def RunScript(self, frame, speed, zone, feedback):
+        if not frame:
+            add_warning('Frame is not defined. The component did not run.')
+            return (None)
+
+        if not speed or speed < 0.01:
+            add_warning('Speed must be greated or equals to 0.01 mm/s')
+            return (None)
+
+        if not zone:
+            zone = rrc.Zone.FINE
+
+        frame = coerce_frame(frame)
+        feedback = feedback or rrc.FeedbackLevel.NONE
+        ins = rrc.MoveToFrame(frame, speed, zone, motion_type=self.motion_type, feedback_level=feedback)
+
+        return (ins)
+
+    def OnTextMenuClick(self, _sender, _args):
+        try:
+            self.motion_type = rrc.Motion.JOINT if self.motion_type == rrc.Motion.LINEAR else rrc.Motion.LINEAR
+            self.ExpireSolution(True)
+        except Exception as ex:
+            add_error(str(ex))
+
+    def AppendAdditionalMenuItems(self, items):
+        component.AppendAdditionalMenuItems(self, items)
+
+        try:
+            image = None
+            item = items.Items.Add('Move in joint space', image, self.OnTextMenuClick)
+            item.Checked = self.motion_type == rrc.Motion.JOINT
+        except Exception as ex:
+            add_error(str(ex))
---
+++ src/compas_rrc/ghpython/components/CompasRrc_RrcClient/code.py
@@ -0,0 +1,23 @@
+"""
+Creates an RRC client instance.
+"""
+from ghpythonlib.component import add_warning
+from ghpythonlib.componentbase import executingcomponent as component
+
+import compas_rrc as rrc
+
+
+class CompasRrcClient(component):
+    def RunScript(self, ros_client, namespace):
+        if not ros_client:
+            add_warning('ros_client is not assigned. The component did not run.')
+            self.Message = ''
+            return (None)
+
+        kwargs = {}
+        if namespace: kwargs['namespace'] = namespace
+
+        rrc_client = rrc.AbbClient(ros_client, **kwargs)
+        self.Message = 'RRC Client ready'
+
+        return (rrc_client)
